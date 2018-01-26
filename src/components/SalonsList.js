36ff0d56c971ca7ed09bee19df537893cc3a4314
magnusohrstrom <magnusohrstrom330@gmail.com>
2018-01-26 20:48:21 +0100

    Add: Sass compile

---
+++ src/components/SalonsList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SalonsList extends Component {
+    render() {
+        return (
+        <section className='salons-list'>
+            
+        </section>
+        );
+    }
+}
+
+export default SalonsList;
