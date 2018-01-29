a9873b664568da7637ad53fd4a59045a198c8642
magnusohrstrom <magnusohrstrom330@gmail.com>
2018-01-29 17:57:11 +0100

    Add: animations

--- src/components/SalonList.js
+++ src/components/SalonList.js
@@ -3,6 +3,7 @@ import SalonListItem from "./SalonListItem";
 import SingleSalon from "./SingleSalon";
 import Header from "./Header";
 import data from '../data.json';
+import { Transition, TransitionGroup } from 'react-transition-group';
 
 class SalonList extends Component {
     state = {
@@ -51,37 +52,45 @@ class SalonList extends Component {
         });
 
         return (
-        <section className='salon-list-section'>
-            {!currentSalon && <Header/>}
-            <ul className='salon-list'>
-                {!currentSalon &&
-                <div className="select-wrapper">
-                    <select onChange = {this.handleSelectOnChange}>
-                        <option value = {[200,300,400,500,600,700,800,900,1000,1100]} >Alla prisklasser</option>
-                        <option value = {[200,300]} >Pris 200 kr - 300kr</option>
-                        <option value = {[400,500]} >Pris 400 kr - 500kr</option>
-                        <option value = {[600,700]} >Pris 600 kr - 700kr</option>
-                        <option value = {[800,900,1000,1100]} >Pris 800 kr och uppåt </option>
-                    </select>
-                    <svg width="12px" height="7px" viewBox="0 0 12 7" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"> <desc>Created with Sketch.</desc> <defs></defs> <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd"> <g id="Lista" transform="translate(-348.000000, -87.000000)" stroke="#B69F58"> <polyline id="Path-2-Copy-4" transform="translate(353.839435, 90.455857) rotate(90.000000) translate(-353.839435, -90.455857) " points="351.446027 85 356.67887 90.2328434 351 95.9117134"></polyline> </g> </g> </svg>
-                </div>    
-                }
-                {!currentSalon && renderListOfSalons}
-                {currentSalon && <SingleSalon 
-                    onClick = {this.closeSingleSalonAndOpenListOnClick}
-                    name = {currentSalon.name}
-                    adress = {currentSalon.adress}
-                    openUntil = {currentSalon.openingHours[1]}
-                    websiteUrl = {currentSalon.websiteUrl}
-                    presentationText = {currentSalon.presentationText}
-                    teleNumber = {currentSalon.teleNumber}
-                    mapsUrl = {currentSalon.mapsUrl}
-                    imageUrl = {currentSalon.imageUrl}
-                    rating = {currentSalon.rating}
-                />}
-                
-            </ul>
-        </section>
+        <TransitionGroup
+        transitionName="salon-list-transition"
+        transitionAppear={true}
+        transitionAppearTimeout={300}
+        transitionEnter={false}
+        transitionLeave={false}
+        >    
+            <section className='salon-list-section'>
+                {!currentSalon && <Header/>}
+                <ul className='salon-list'>
+                    {!currentSalon &&
+                    <div className="select-wrapper">
+                        <select onChange = {this.handleSelectOnChange}>
+                            <option value = {[200,300,400,500,600,700,800,900,1000,1100]} >Alla prisklasser</option>
+                            <option value = {[200,300]} >Pris 200 kr - 300kr</option>
+                            <option value = {[400,500]} >Pris 400 kr - 500kr</option>
+                            <option value = {[600,700]} >Pris 600 kr - 700kr</option>
+                            <option value = {[800,900,1000,1100]} >Pris 800 kr och uppåt </option>
+                        </select>
+                        <svg width="12px" height="7px" viewBox="0 0 12 7" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"> <desc>Created with Sketch.</desc> <defs></defs> <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd"> <g id="Lista" transform="translate(-348.000000, -87.000000)" stroke="#B69F58"> <polyline id="Path-2-Copy-4" transform="translate(353.839435, 90.455857) rotate(90.000000) translate(-353.839435, -90.455857) " points="351.446027 85 356.67887 90.2328434 351 95.9117134"></polyline> </g> </g> </svg>
+                    </div>    
+                    }
+                    {!currentSalon && renderListOfSalons}
+                    {currentSalon && <SingleSalon 
+                        onClick = {this.closeSingleSalonAndOpenListOnClick}
+                        name = {currentSalon.name}
+                        adress = {currentSalon.adress}
+                        openUntil = {currentSalon.openingHours[1]}
+                        websiteUrl = {currentSalon.websiteUrl}
+                        presentationText = {currentSalon.presentationText}
+                        teleNumber = {currentSalon.teleNumber}
+                        mapsUrl = {currentSalon.mapsUrl}
+                        imageUrl = {currentSalon.imageUrl}
+                        rating = {currentSalon.rating}
+                    />}
+                    
+                </ul>
+            </section>
+        </TransitionGroup>
         );
     }
 }
